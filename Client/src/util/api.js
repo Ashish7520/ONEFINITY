04e568a074f88676f38da54d2dabcb9140c16814
Ashish7520 <ashishnandwana2@gmail.com>
2024-01-20 22:10:16 +0530

    update api.js

--- Client/src/util/api.js
+++ Client/src/util/api.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/task",
+  baseURL: "https://onefinity-api-file.vercel.app/task",
 });
 
 export const getAllTasks = async () => {
